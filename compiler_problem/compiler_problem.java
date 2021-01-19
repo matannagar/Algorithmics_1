f24baa577511ea6c8614de5224470499904a04f3
matannagar <73398186+matannagar@users.noreply.github.com>
2021-01-19 12:58:56 +0200

    "added compiler_problem"

--- compiler_problem/compiler_problem.java
+++ compiler_problem/compiler_problem.java
@@ -1,14 +1,16 @@
 package compiler_problem;
 
+import java.util.Arrays;
+
 public class compiler_problem {
     public static void main(String[] args) {
-        Program p1 = new Program("a1",6,6);
-        Program p2 = new Program("a2",9,5);
-        Program p3 = new Program("a3",2,14);
-        Program p4 = new Program("a4",3,13);
-        Program p5 = new Program("a5",11,10);
+        Program p1 = new Program("a1", 3, 12);
+        Program p2 = new Program("a2", 3, 3);
+        Program p3 = new Program("a3", 3, 4);
+        Program p4 = new Program("a4", 3, 3);
+        Program p5 = new Program("a5", 3, 6);
 
-        Program[] arr= new Program[5];
+        Program[] arr = new Program[5];
         arr[0] = p1;
         arr[1] = p2;
         arr[2] = p3;
@@ -31,6 +33,7 @@ public class compiler_problem {
             this.len = len;
             this.freq = freq;
         }
+
         @Override
         public String toString() {
             return "[" + name + " ," + len + " ," + freq + "]";
@@ -38,7 +41,7 @@ public class compiler_problem {
     }
 
     public static void getOptimalOrder(Program[] programs) {
-        mergeSort(programs, 0, programs.length);
+        sort_when_len_is_equal(programs, 0, programs.length);
         int totalTime = 0;
         int totalLen = 0;
         for (Program program : programs) {
@@ -55,7 +58,7 @@ public class compiler_problem {
 
         int mid = (low + high) / 2;
         mergeSort(programs, low, mid);
-        mergeSort(programs, mid+1, high);
+        mergeSort(programs, mid, high);
 
         int i = low, j = mid, k = 0;
 
@@ -71,4 +74,51 @@ public class compiler_problem {
 
         System.arraycopy(temp, 0, programs, low, n);
     }
+
+    private static void sort_when_freq_is_equal(Program[] programs, int low, int high) {
+        int n = high - low;
+        if (n <= 1) return;
+
+        int mid = (low + high) / 2;
+        sort_when_freq_is_equal(programs, low, mid);
+        sort_when_freq_is_equal(programs, mid, high);
+
+        int i = low, j = mid, k = 0;
+
+        Program[] temp = new Program[n];
+        while (i < mid && j < high) {
+            double t1 =  programs[i].len;
+            double t2 =  programs[j].len;
+            if (t1 < t2) temp[k++] = programs[i++];
+            else temp[k++] = programs[j++];
+        }
+        while (i < mid) temp[k++] = programs[i++];
+        while (j < high) temp[k++] = programs[j++];
+
+        System.arraycopy(temp, 0, programs, low, n);
+    }
+
+    private static void sort_when_len_is_equal(Program[] programs, int low, int high) {
+        int n = high - low;
+        if (n <= 1) return;
+
+        int mid = (low + high) / 2;
+        sort_when_len_is_equal(programs, low, mid);
+        sort_when_len_is_equal(programs, mid, high);
+
+        int i = low, j = mid, k = 0;
+
+        Program[] temp = new Program[n];
+        while (i < mid && j < high) {
+            double t1 =  programs[i].freq;
+            double t2 =  programs[j].freq;
+            if (t1 < t2) temp[k++] = programs[i++];
+            else temp[k++] = programs[j++];
+        }
+        while (i < mid) temp[k++] = programs[i++];
+        while (j < high) temp[k++] = programs[j++];
+
+        System.arraycopy(temp, 0, programs, low, n);
+    }
 }
+
